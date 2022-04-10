c2729f41ee9e1a1f77e6d1aa964fc0c1d21a1b2d
user <user@email.com>
2022-04-11 00:03:32 +0100

    deploy

---
+++ precache-manifest.8330d75164af222e20636109d2a0a3d3.js
@@ -0,0 +1,178 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e33c259ae0f946500877779097be4ab8",
+    "url": "/404.html"
+  },
+  {
+    "revision": "35ccd810aceb293786ac07d1888a71b6",
+    "url": "/LICENSE.md"
+  },
+  {
+    "revision": "3a6dd6f670b80de16caa3c05f28d0abd",
+    "url": "/README.md"
+  },
+  {
+    "revision": "2ade6b2508aeff65be47",
+    "url": "/css/chunk-vendors.f9e3a8b1.css"
+  },
+  {
+    "revision": "b97bc055d829b1060e2a",
+    "url": "/css/index.65b73714.css"
+  },
+  {
+    "revision": "a83df317dd89c7dd5388a152a26b2236",
+    "url": "/fonts/Play-Regular.a83df317.ttf"
+  },
+  {
+    "revision": "1a575a4138e5f366474f0e7c5bd614a5",
+    "url": "/fonts/fa-brands-400.1a575a41.woff"
+  },
+  {
+    "revision": "ed311c7a0ade9a75bb3ebf5a7670f31d",
+    "url": "/fonts/fa-brands-400.ed311c7a.woff2"
+  },
+  {
+    "revision": "b91d376b8d7646d671cd820950d5f7f1",
+    "url": "/fonts/fa-regular-400.b91d376b.woff2"
+  },
+  {
+    "revision": "d1d7e3b4c219fde0f7376c6facfd7149",
+    "url": "/fonts/fa-regular-400.d1d7e3b4.woff"
+  },
+  {
+    "revision": "d745348d289b149026921f197929a893",
+    "url": "/fonts/fa-solid-900.d745348d.woff"
+  },
+  {
+    "revision": "d824df7eb2e268626a2dd9a6a741ac4e",
+    "url": "/fonts/fa-solid-900.d824df7e.woff2"
+  },
+  {
+    "revision": "e04efa7527460e6d38b1ba79b405761b",
+    "url": "/fonts/flUhRq6tzZclQEJ-Vdg-IuiaDsNa.e04efa75.woff"
+  },
+  {
+    "revision": "3e82d9a3cad4ab32b96d5589d55db0e6",
+    "url": "/fonts/flUhRq6tzZclQEJ-Vdg-IuiaDsNcIhQ8tQ.3e82d9a3.woff2"
+  },
+  {
+    "revision": "2cd462f936c27120021918882f34d16f",
+    "url": "/fonts/gok-H7zzDkdnRel8-DQ6KAXJ69wP1tGnf4ZGhUcY.2cd462f9.woff"
+  },
+  {
+    "revision": "6d72bbd3267f0ab52a76a0c59517c79b",
+    "url": "/fonts/gok-H7zzDkdnRel8-DQ6KAXJ69wP1tGnf4ZGhUcel5euIg.6d72bbd3.woff2"
+  },
+  {
+    "revision": "bca2cb23e3f5ba293f27f9219ff4f96b",
+    "url": "/img/01.bca2cb23.png"
+  },
+  {
+    "revision": "5cb8f02bcbc0a2830485f77b05e957b4",
+    "url": "/img/02.5cb8f02b.png"
+  },
+  {
+    "revision": "8a1a28bb8eb4ecd63ea28dc8f5f4bf90",
+    "url": "/img/03.8a1a28bb.png"
+  },
+  {
+    "revision": "9b01680b2723533f74cefe4d036bb520",
+    "url": "/img/04.9b01680b.png"
+  },
+  {
+    "revision": "4821a48474bffd7cb922066557de6c2c",
+    "url": "/img/05.4821a484.png"
+  },
+  {
+    "revision": "610b9d46bdf60e601fceee26c1ebe36e",
+    "url": "/img/banner.610b9d46.png"
+  },
+  {
+    "revision": "00f25396ea29dae1188c56c836efb33a",
+    "url": "/img/demoqrcode.png"
+  },
+  {
+    "revision": "2321c37b8ce832edbc23adac8aff7b4e",
+    "url": "/img/leavehomehappy.png"
+  },
+  {
+    "revision": "4fdafa39afd6dfe0a69049ecf432d87b",
+    "url": "/img/logo.4fdafa39.png"
+  },
+  {
+    "revision": "e559d229420c36d8bb5af45cee0d8914",
+    "url": "/img/noCard.e559d229.png"
+  },
+  {
+    "revision": "107c023e090d9854f9ab1d4487c4ec5a",
+    "url": "/img/postCert.107c023e.png"
+  },
+  {
+    "revision": "d26ee7878057c9dda9bb1c9c0defd65e",
+    "url": "/img/preCert.d26ee787.png"
+  },
+  {
+    "revision": "66994d699dd816438008f8f17ad21593",
+    "url": "/img/preTest.66994d69.png"
+  },
+  {
+    "revision": "8cd8876ba071212f353957dba82664df",
+    "url": "/img/splashscreens/ipad_splash.png"
+  },
+  {
+    "revision": "b5a18eef9a8c646be12a050a08f5f2aa",
+    "url": "/img/splashscreens/ipadpro1_splash.png"
+  },
+  {
+    "revision": "f042265fd86fcb42b5bc27e160295e42",
+    "url": "/img/splashscreens/ipadpro2_splash.png"
+  },
+  {
+    "revision": "ccc55c51669a64acc4bbede128b1fe4d",
+    "url": "/img/splashscreens/ipadpro3_splash.png"
+  },
+  {
+    "revision": "e92d4e3b45d6d689f9cd3bb7120e5574",
+    "url": "/img/splashscreens/iphone5_splash.png"
+  },
+  {
+    "revision": "a3b6505b1690844fbb53917fd383cdb5",
+    "url": "/img/splashscreens/iphone6_splash.png"
+  },
+  {
+    "revision": "9e48a0e9b1384525abe2ca6d8df689d1",
+    "url": "/img/splashscreens/iphoneplus_splash.png"
+  },
+  {
+    "revision": "d9d3430425fc379b0e623f768a0d4d5e",
+    "url": "/img/splashscreens/iphonex_splash.png"
+  },
+  {
+    "revision": "26d307555867f653c6c6888db2033227",
+    "url": "/img/splashscreens/iphonexr_splash.png"
+  },
+  {
+    "revision": "5d7c313b06d4adaf94eff4ab7c674443",
+    "url": "/img/splashscreens/iphonexsmax_splash.png"
+  },
+  {
+    "revision": "75e5c09fe469d38c46ccc544b5aeb939",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2ade6b2508aeff65be47",
+    "url": "/js/chunk-vendors.0988aab9.js"
+  },
+  {
+    "revision": "b97bc055d829b1060e2a",
+    "url": "/js/index.22c5fc44.js"
+  },
+  {
+    "revision": "a97e47cb73359f4e7971f0a2eb01047b",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
